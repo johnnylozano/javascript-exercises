15172462dd9dc80ea162e59f9771374d2ccf9f3d
johnnylozano <102393958+johnnylozano@users.noreply.github.com>
2022-10-03 01:16:10 -0400

    Added test case and wrote palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,34 @@
-const palindromes = function () {
+const palindromes = function (string) {
+   if (string === "" || string === null) {
+      return "ERROR";
+   }
+   string = string
+      .replace(/[^\w\s\']|_/g, "")
+      .replace(/\s+/g, "")
+      .toLowerCase();
 
+   if (string.length % 2 !== 0) {
+      return (
+         string.substring(0, string.length / 2) ===
+         string
+            .substring(string.length, string.length / 2 + 1)
+            .split("")
+            .reverse()
+            .join("")
+      );
+   } else {
+      return (
+         string.substring(0, string.length / 2) ===
+         string
+            .substring(string.length, string.length / 2)
+            .split("")
+            .reverse()
+            .join("")
+      );
+   }
 };
+
+console.log(palindromes(""));
 
 // Do not edit below this line
 module.exports = palindromes;
